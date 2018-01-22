d494ea0b8da850be2a5011d83ae99568be55c404
Simon Constans <simonc@linux.com>
2018-01-22 23:17:10 +0100

    feat: Get Github token 🗝

--- src/github.js
+++ src/github.js
@@ -1,4 +1,9 @@
 import inquirer from 'inquirer'
+import Preferences from 'preferences'
+import CLI from 'clui'
+import octokit from '@octokit/rest'
+
+const github = octokit()
 
 export const getGithubCredentials = async () => {
   const questions = [
@@ -29,4 +34,50 @@ export const getGithubCredentials = async () => {
   ]
 
   return inquirer.prompt(questions)
+}
+
+export const getGithubToken = async () => {
+  const prefs = new Preferences('cozy-konnector-monitor')
+
+  if (prefs.github && prefs.github.token) {
+    return prefs.github.token
+  }
+
+  const credentials = await getGithubCredentials()
+
+  const Spinner = CLI.Spinner
+  const status = new Spinner('Authenticating you, please wait...')
+  status.start()
+
+  github.authenticate({ type: 'basic', ...credentials })
+  try {
+    const result = await github.authorization.create({
+      scopes: ['user', 'public_repo', 'repo', 'repo:status'],
+      note: 'cozy-konnector-monitor, the command-line tool for monitoring konnectors',
+      note_url: 'https://github.com/cozy/cozy-konnector-monitor'
+    })
+    status.stop()
+
+    const token = result.data && result.data.token
+    if (token) {
+      prefs.github = { token }
+      return token
+    }
+  } catch (e) {
+    status.stop()
+
+    const message = JSON.parse(e.message)
+
+    if (message.message === 'Bad credentials') {
+      console.log('##')
+      console.warn(` ⚠️  ${message.message}`)
+      console.log()
+
+      return await getGithubToken()
+    } else if (message.errors && message.errors.length > 0 && message.errors[0].code === 'already_exists') {
+      throw new Error(`Token is already exists, you can remove it: https://github.com/settings/tokens`)
+    } else {
+      throw new Error(message.message)
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import clear from 'clear'
 import chalk from 'chalk'
 import figlet from 'figlet'
 
-import { getGithubCredentials } from './github'
+import { getGithubToken } from './github'
 
 const main = async () => {
   clear()
@@ -12,8 +12,14 @@ const main = async () => {
     )
   )
 
-  const credentials = await getGithubCredentials()
-  console.log(credentials)
+  try {
+    const token = await getGithubToken()
+    console.log(token)
+  } catch (e) {
+    console.log()
+    console.warn(` ⚠️  ${e.message || e}`)
+    console.log()
+  }
 }
 
 main()
