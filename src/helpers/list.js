e53ab44648c80dad17d9a3828ddbe893af7eb12f
Simon Constans <simonc@linux.com>
2018-01-24 21:50:01 +0100

    feat: Select which konnector we want test 🤙

--- src/helpers/list.js
+++ src/helpers/list.js
@@ -8,7 +8,7 @@ const REPO_REGEXP = /^https?:\/\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0
 const PROVIDER_GITHUB = 'github.com'
 const PROVIDER_GITLAB = 'gitlab.cozycloud.cc'
 
-const parsingRepository = url => {
+export const parsingRepository = url => {
   const matching = url.match(REPO_REGEXP)
   return {
     provider: matching[1] === PROVIDER_GITHUB ? 'Github' : 'Gitlab',
--- src/questions.js
+++ src/questions.js
@@ -1,26 +1,47 @@
 import inquirer from 'inquirer'
 import { pulls } from './pulls'
+import { getRepositories } from './helpers/list'
+
+const selectKonnector = async () => {
+  const name = 'selected'
+  const repos = getRepositories()
+  const question = {
+    type: 'list',
+    name,
+    message: 'Which konnector do you want to test?',
+    choices: repos.map(info => info.repoName)
+  }
+  const answer = (await inquirer.prompt(question))[name]
+  console.log()
+  console.log(answer)
+}
 
 export const starter = async () => {
   const name = 'starter'
-  const choice1 = 'Pull all konnectors'
-  const choice2 = 'Quit'
+  const pull = 'Pull all konnectors'
+  const testOne = 'Test a konnector'
+  const quit = 'Quit'
 
   const question = {
     type: 'list',
     name,
     message: 'What do you want to do?',
-    choices: [choice1, choice2]
+    choices: [pull, testOne, quit]
   }
 
   let answer
-  while (answer !== choice2) {
+  while (answer !== quit) {
     console.log()
     answer = (await inquirer.prompt(question))[name]
     console.log()
 
-    if (answer === choice1) {
-      await pulls()
+    switch (answer) {
+      case pull:
+        await pulls()
+        break;
+      case testOne:
+        await selectKonnector()
+        break;
     }
   }
 }
