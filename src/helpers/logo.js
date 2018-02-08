3a5940209223eba8f2a39c83b09d882f654042e7
Simon Constans <simonc@linux.com>
2018-02-08 11:20:16 +0100

    feat: Add version number on header 🔍

--- src/helpers/logo.js
+++ src/helpers/logo.js
@@ -1,10 +1,12 @@
 import clear from "clear"
 import chalk from "chalk"
 import figlet from "figlet"
+import { version } from "../../package.json"
 
 export const displayLogo = () => {
   clear()
   console.log(
     chalk.yellow(figlet.textSync("konitor", { horizontalLayout: "full" }))
   )
+  console.log(` v${version}\n`)
 }
