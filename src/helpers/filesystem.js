749738aabfc539df675bad3a4fa7d694acecc745
Simon Constans <simonc@linux.com>
2018-03-20 18:35:45 +0100

    bug: why travis don't display pdf 🧐

--- src/helpers/filesystem.js
+++ src/helpers/filesystem.js
@@ -2,6 +2,7 @@ import fs from 'fs-extra'
 
 export const getFilesFromDir = async (dir, extension) => {
   const dirCont = await fs.readdir(dir)
+  console.log('dirCont', dirCont)
   const re = new RegExp(`.*\.(${extension})`, 'ig') // eslint-disable-line no-useless-escape
   const files = dirCont.filter(elm => elm.match(re))
 
