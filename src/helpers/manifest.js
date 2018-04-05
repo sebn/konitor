205e73ebddae245a77d1bb0446374b47fd08d48a
Simon Constans <simonc@linux.com>
2018-04-05 23:03:11 +0200

    feat: Update after field modification ✌️

--- src/helpers/manifest.js
+++ src/helpers/manifest.js
@@ -9,12 +9,28 @@ export const getManifest = async path => {
 export const getFields = async path => {
   const manifest = await getManifest(path)
 
-  return (
+  const oldFormat =
     manifest.fields &&
     manifest.fields.account &&
     manifest.fields.account.accountFormat &&
     manifest.fields.account.accountFormat.split(',')
-  )
+
+  if (oldFormat) {
+    return oldFormat
+  }
+
+  const fields = []
+  Object.keys(manifest.fields).forEach(k => {
+    if (
+      k !== 'advancedFields' &&
+      (manifest.fields[k].isRequired === undefined ||
+        manifest.fields[k].isRequired === true)
+    ) {
+      fields.push(k)
+    }
+  })
+
+  return fields
 }
 
 export const getSlug = async path => {
